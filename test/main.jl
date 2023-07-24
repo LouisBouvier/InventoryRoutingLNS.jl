bfa5f8cea61a9d74555f5f55333ac0520d98fb66
LouisBouvier <lbouvier975@gmail.com>
2023-07-24 18:15:20 +0200

    added the main.jl file to reproduce the numerical experiments of the paper with the best set of hyper parameters for 90 minutes

--- test/main.jl
+++ test/main.jl
@@ -1,1 +1,23 @@
-# TODO: fill this
+using DataDeps
+using Gurobi
+using InventoryRoutingLNS
+using Random
+Random.seed!(60)
+
+folder_path = joinpath(datadep"IRP-instances", "instances", "instances")
+list_of_instances = readdir(folder_path)
+
+
+for instance_id in list_of_instances
+    println("Solving instance $instance_id")
+    instance = read_instance_CSV(joinpath(folder_path, instance_id))
+    paper_matheuristic!(
+        instance;
+        n_it_commodity_reinsertion=10,
+        n_it_customer_reinsertion=instance.C,
+        tol=-0.01,
+        time_limit=90.0,
+        verbose=true,
+        optimizer = Gurobi.Optimizer
+    )
+end
