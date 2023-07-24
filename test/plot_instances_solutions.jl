a2c77961e2f7f817fd39323ef8cb64d751850ab1
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-07-24 15:23:19 +0200

    Fix formatting

--- test/plot_instances_solutions.jl
+++ test/plot_instances_solutions.jl
@@ -5,16 +5,30 @@ using InventoryRoutingLNS
 folder_path = joinpath(datadep"IRP-instances", "instances", "instances")
 list_of_instances = readdir(folder_path)
 
-solutions_path_lns = joinpath(datadep"IRP-solutions", "solutions", "solutions", "LNS_smax3_tl90_co10_cumax", "solutions", "LNS")
-solutions_path_initls = joinpath(datadep"IRP-solutions", "solutions", "solutions", "LNS_smax3_tl90_co10_cumax", "solutions", "init_ls")
+solutions_path_lns = joinpath(
+    datadep"IRP-solutions",
+    "solutions",
+    "solutions",
+    "LNS_smax3_tl90_co10_cumax",
+    "solutions",
+    "LNS",
+)
+solutions_path_initls = joinpath(
+    datadep"IRP-solutions",
+    "solutions",
+    "solutions",
+    "LNS_smax3_tl90_co10_cumax",
+    "solutions",
+    "init_ls",
+)
 
 for instance_id in list_of_instances
     ## Open instance
-    println("Open instance "*instance_id)
+    println("Open instance " * instance_id)
     instance = read_instance_CSV(joinpath(folder_path, instance_id))
     ## Open solution 
-    solution_lns = read_solution(joinpath(solutions_path_lns, instance_id*".txt"))
-    solution_init = read_solution(joinpath(solutions_path_initls, instance_id*".txt"))
+    solution_lns = read_solution(joinpath(solutions_path_lns, instance_id * ".txt"))
+    solution_init = read_solution(joinpath(solutions_path_initls, instance_id * ".txt"))
     analyze_instance(instance)
     analyze_solution(instance, solution_lns, solution_init)
     # compute detailed cost init
